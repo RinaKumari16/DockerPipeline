75e780a64d1c343edda776f3e35cb8d2b051a83e
Rina Kumari <57893629+RinaKumari16@users.noreply.github.com>
2020-10-06 12:34:04 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+package devops_testing.devops_training_code;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
